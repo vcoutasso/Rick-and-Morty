dbe285f439a2e047fec3bd8b903b45b8f41ee981
Vinícius Couto Tasso <vcoutasso@gmail.com>
2021-10-14 17:49:54 -0300

    First commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Vinícius Couto on 14/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
